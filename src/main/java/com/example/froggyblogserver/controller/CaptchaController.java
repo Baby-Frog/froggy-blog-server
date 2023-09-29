55b96b6fe9e43d9d2c55bc80110be56eb91b087d
vovanviet <vietvvth2107012@fpt.edu.vn>
2023-09-29 16:59:04 +0700

    😥 remove redundant

--- src/main/java/com/example/froggyblogserver/controller/CaptchaController.java
+++ src/main/java/com/example/froggyblogserver/controller/CaptchaController.java
@@ -1,36 +1,8 @@
 package com.example.froggyblogserver.controller;
 
-
-import com.example.froggyblogserver.common.CONSTANTS;
-import com.example.froggyblogserver.common.MESSAGE;
-import com.example.froggyblogserver.exception.ValidateInputException;
-import com.example.froggyblogserver.response.BaseResponse;
 import com.example.froggyblogserver.utils.RecaptchaUtils;
-import com.google.code.kaptcha.Producer;
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import kotlin.jvm.internal.TypeParameterReference;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
