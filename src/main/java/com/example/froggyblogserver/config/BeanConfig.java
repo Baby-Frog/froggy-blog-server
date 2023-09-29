2103679ac3f091185253fe8a794d8fd6c708b013
vovanviet <vietvvth2107012@fpt.edu.vn>
2023-09-29 11:32:01 +0700

    😪 update captcha config

--- src/main/java/com/example/froggyblogserver/config/BeanConfig.java
+++ src/main/java/com/example/froggyblogserver/config/BeanConfig.java
@@ -23,14 +23,14 @@ public class BeanConfig {
         // Cấu hình các thuộc tính của Captcha
         properties.setProperty("kaptcha.border", "yes");
         properties.setProperty("kaptcha.border.color", "105,179,90");
-        properties.setProperty("kaptcha.textproducer.font.color", "228,233,23");
+        properties.setProperty("kaptcha.textproducer.font.color", "40,38,38");
         properties.setProperty("kaptcha.image.width", "200");
         properties.setProperty("kaptcha.image.height", "60");
         properties.setProperty("kaptcha.textproducer.font.size", "40");
         properties.setProperty("kaptcha.session.key", "captchaCode");
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.textproducer.char.length", "6");
         properties.setProperty("kaptcha.textproducer.char.space", "5");
-        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.ShadowGimpy");
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
         properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
         properties.setProperty("kaptcha.textproducer.impl", "com.google.code.kaptcha.text.impl.DefaultTextCreator");
 
